1d29f9fa49c74cf520549721208f328bdebff5e0
SergKaravaev <91072914+SergKaravaev@users.noreply.github.com>
2024-10-24 16:57:25 +0300

    Add files via upload

---
+++ work/demoOne/src/main/java/com/example/demoOne/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.example.demoOne.repository;
+
+import com.example.demoOne.bd.UserBd;
+import com.example.demoOne.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+    private final UserBd userBd;
+
+    public User getById(Long userId) {
+        User user = userBd.getUsers().get(userId);
+        return user;
+    }
+
+    public User getUserByFullName(String firstName, String lastName) {
+        for (User user : userBd.getUsers().values()) {
+            if (user.getFirstName().equals(firstName) && user.getLastName().equals(lastName)) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
---
+++ work/demoOne/src/main/java/com/example/demoOne/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.demoOne.service;
+
+import com.example.demoOne.dto.UserRequestDto;
+import com.example.demoOne.entity.User;
+import com.example.demoOne.mapper.UserMapper;
+import com.example.demoOne.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserRequestDto getUserById(Long userId) {
+        User user = userRepository.getById(userId);
+        UserRequestDto userRequestDto = userMapper.toDto(user);
+        return userRequestDto;
+    }
+
+    public UserRequestDto getUserByFullName(String firstName, String lastName) {
+        User user = userRepository.getUserByFullName(firstName, lastName);
+        UserRequestDto userRequestDto = userMapper.toDto(user);
+        return userRequestDto;
+    }
+}
