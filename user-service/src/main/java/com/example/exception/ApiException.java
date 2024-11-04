4d3ed4c4a40256b6605673b2ab75e36a66c8253a
skaravaev <sergey.karavaev@artezio.com>
2024-11-04 15:33:58 +0300

    main: added exception handling

--- user-service/src/main/java/com/example/exception/ApiException.java
+++ user-service/src/main/java/com/example/exception/ApiException.java
@@ -1,11 +1,58 @@
 package com.example.exception;
 
+import lombok.Setter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+@Setter
 public class ApiException extends ResponseStatusException {
 
-    public ApiException(HttpStatus status, String message) {
-        super(status, message);
+    private final transient Object[] args;
+    private String path;
+
+    public ApiException(HttpStatus status, String reason, Object... args) {
+        super(status, reason);
+        this.args = args;
+    }
+
+    public ApiException(HttpStatus status, String reason, String path, Object... args) {
+        this(status, reason, args);
+        this.path = path;
+    }
+
+    public Map<String, Object> getResponseBodyFields(MessageSource messageSource, Locale locale) {
+        String message = getMessageFromSource(messageSource, locale);
+        return createResponseBody(message);
+    }
+
+    private String getMessageFromSource(MessageSource messageSource, Locale locale) {
+        try {
+            return messageSource.getMessage(Objects.requireNonNull(getReason()), args, locale);
+        } catch (NoSuchMessageException e) {
+            return getReason();
+        }
+    }
+
+    private Map<String, Object> createResponseBody(String message) {
+        int statusCode = getStatusCode().value();
+        Map<String, Object> fields = new LinkedHashMap<>();
+        fields.put("timestamp", new Date());
+        fields.put("status", statusCode);
+        fields.put("error", message);
+        fields.put("path", path);
+        return fields;
+    }
+
+    public ResponseEntity<Map<String, Object>> toLocalResponseEntity(MessageSource messageSource, Locale locale) {
+        return ResponseEntity.status(getStatusCode()).body(getResponseBodyFields(messageSource, locale));
     }
 }
--- user-service/src/main/java/com/example/exception/ApiExceptionHandler.java
+++ user-service/src/main/java/com/example/exception/ApiExceptionHandler.java
@@ -1,15 +1,44 @@
 package com.example.exception;
 
+import feign.FeignException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
-public class ApiExceptionHandler {
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+@Slf4j
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ApiExceptionHandler {
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Map<String, Object>> handleApiExceptions(ApiException ex,
+                                                                   Locale locale, HttpServletRequest request) {
+        ex.setPath(extractQueryPath(request));
+        return ex.toLocalResponseEntity(messageSource, locale);
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<Map<String, Object>> handleFeignException(FeignException ex,
+                                                                    Locale locale, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.valueOf(ex.status());
+        ApiException wrapper = new ApiException(httpStatus, httpStatus.getReasonPhrase(), extractQueryPath(request));
+        log.error(ex.getMessage(), ex);
+        return wrapper.toLocalResponseEntity(messageSource, locale);
+    }
+
+    private String extractQueryPath(HttpServletRequest request) {
+        return Objects.nonNull(request.getQueryString()) ?
+                String.format("%s?%s", request.getRequestURI(), request.getQueryString()) :
+                request.getRequestURI();
     }
 }
