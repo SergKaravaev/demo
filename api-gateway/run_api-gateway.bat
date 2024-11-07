8c61112f264bc505a2079aaddddf5ff7680b9a7a
skaravaev <sergey.karavaev@artezio.com>
2024-11-07 16:49:59 +0300

    main: add eureka-service

--- api-gateway/run_api-gateway.bat
+++ api-gateway/run_api-gateway.bat
@@ -1,5 +1,8 @@
 call ..\config-service\run_config-service.bat
 
+cd ..\eureka-service
+docker-compose up --build -d
+
 cd ..\user-service
 docker-compose up --build -d
 
--- config-service/run_config-service.bat
+++ config-service/run_config-service.bat
@@ -1,5 +1,4 @@
 docker network inspect demo-network >nul 2>&1 || docker network create demo-network
 cd ..\config-service
 docker-compose up --build -d
-docker-compose ps
 
